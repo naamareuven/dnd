562f866fd89a6560700ffb8f0670affb4099a5af
naamareuven <naamareuven8@gmail.com>
2020-12-08 15:44:12 +0100

    debug

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloProvider } from "react-apollo";
-import App from "./containers/App";
-import client from "./utils/apolloClient";
+import { ApolloProvider } from "@apollo/client";
+import {App} from "./containers/App";
+import {client} from "./utils/apolloClient";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,7 +1,4 @@
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
-
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
